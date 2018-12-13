2e9a9a1675cf7be70b31b8c808a0533118f23b19
Ozemko <aleksey.ozemko@sigmaukraine.com>
2018-12-13 12:55:47 +0200

    add UserController

---
+++ SeedAPI.Web.API/SeedAPI.Web.API/Api/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SeedAPI.Maps;
+using SeedAPI.ViewModels;
+
+namespace SeedAPI.Web.API.Api
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        readonly IUserMap userMap;
+
+        public UserController(IUserMap map)
+        {
+            userMap = map;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserViewModel> Get()
+        {
+            return userMap.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string user)
+        {
+
+        }
+
+        [HttpPut("{id}")]
+
+        public void Put(int id, [FromBody]string user)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
